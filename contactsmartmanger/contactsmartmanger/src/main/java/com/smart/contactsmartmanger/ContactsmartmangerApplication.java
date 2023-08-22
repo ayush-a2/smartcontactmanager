80e42063292e35977f4f904d02edc8b1b45d9e45
ayush-a2 <ayush8583@gmail.com>
2023-08-22 21:07:58 +0530

    entity

---
+++ contactsmartmanger/contactsmartmanger/src/main/java/com/smart/contactsmartmanger/ContactsmartmangerApplication.java
@@ -0,0 +1,13 @@
+package com.smart.contactsmartmanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsmartmangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsmartmangerApplication.class, args);
+	}
+
+}
